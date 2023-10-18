450c9ce669dc809e904f1cb91112fcb4ad528067
Jesse <jesse.lare@gmail.com>
2023-10-17 22:51:27 -0400

    completing index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer');
+const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } = require('./db/queries');
+
+const init = async () => {
+    const { choice } = await inquirer.prompt({
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role',
+            'Exit'
+        ]
+    });
+
+    switch (choice) {
+        case 'View All Departments':
+            await viewAllDepartments();
+            break;
+        case 'View All Roles':
+            await viewAllRoles();
+            break;
+        case 'View All Employees':
+            await viewAllEmployees();
+            break;
+        case 'Add a Department':
+            await addDepartment();
+            break;
+        case 'Add a Role':
+            await addRole();
+            break;
+        case 'Add an Employee':
+            await addEmployee();
+            break;
+        case 'Update an Employee Role':
+            await updateEmployeeRole();
+            break;
+        default:
+            process.exit();
+    }
+
+    init(); // Re-run the main menu
+};
+
+init();
