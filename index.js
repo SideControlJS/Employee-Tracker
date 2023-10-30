6fc63421e25fbbfd1bbf403f32be773103a37955
Jesse <jesse.lare@gmail.com>
2023-10-30 18:27:56 -0400

    updating index.js

--- index.js
+++ index.js
@@ -1,91 +1,37 @@
-import inquirer from 'inquirer';
 import * as queries from './db/queries.js';
-import { addEmployeePrompt } from './prompts.js';
-
-const addDepartmentPrompt = async () => {
-    const { name } = await inquirer.prompt({
-        type: 'input',
-        name: 'name',
-        message: 'Enter the name of the new department:',
-        validate: name => name ? true: 'Department name cannot be empty!'
-    });
-    return name;
-}
-
-const addRolePrompt = async () => {
-    const departments = await queries.getAllDepartments();
-    const departmentChoices = departments.map(dept => ({
-        name: dept.name,
-        value: dept.id  
-    }));
-    
-    const responses = await inquirer.prompt([
-        {
-            type: 'input', 
-            name: 'title',
-            message: 'Enter the title of the role:',
-            validate: title => title ? true : 'Title cannot be empty!'
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'Enter the salary for the role:',
-            validate: salary => {
-                const valid = !isNaN(parseFloat(salary)) && isFinite(salary);
-                return valid || 'Please enter a number';
-            }
-        },
-        {
-            type: 'list',
-            name: 'departmentId',
-            message: 'Select the department for the role:',
-            choices: departmentChoices
-        }
-    ]);
-
-    return {
-        title: responses.title,
-        salary: parseFloat(responses.salary), // converts salary to number
-        departmentId: responses.departmentId
-    };
-};
-
+import * as prompts from './prompts.js';
 
 
 const init = async () => {
-
-    switch (choice) {
+    try {
+      const choice = await prompts.mainMenuPrompt();
+  
+      switch (choice) {
         case 'View All Departments':
-            await queries.viewAllDepartments();
-            break;
-        case 'View All Roles':
-            await queries.viewAllRoles();
-            break;
-        case 'View All Employees':
-            await queries.viewAllEmployees();
-            break;
+          await queries.viewAllDepartments();
+          break;
         case 'Add a Department':
-            const departmentName = await addDepartmentPrompt();
-            await queries.addDepartment(departmentName);
-            break;
+          const departmentName = await prompts.addDepartmentPrompt();
+          await queries.addDepartment(departmentName);
+          break;
         case 'Add a Role':
-            const roleData = await addRolePrompt();
-            await queries.addRole(roleData);
-            break;
-        case 'Add an Employee':
-            const employeeData = await addEmployeePrompt();
-            await queries.addEmployee(employeeData);
-            break;
-        case 'Update an Employee Role':
-            const { employeeId, newRoleId } = await updateEmployeeRolePrompt();
-            await queries.updateEmployeeRole(employeeId, newRoleId);
-            break;
+          const roleData = await prompts.addRolePrompt();
+          await queries.addRole(roleData);
+          break;
+        case 'Exit':
+          console.log('Goodbye!');
+          return;
         default:
-            process.exit();
+          console.log('Invalid choice. Please try again.');
+      }
+  
+      // Recursively call init to show the main menu again
+      init();
+    } catch (error) {
+      console.error('An error occurred:', error);
+      process.exit(1);
     }
-
-    init(); // Re-run the main menu
-};
-
-init();
+  };
+  
+  init();
 
