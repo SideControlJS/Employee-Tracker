12479cebe949597f4cc127b8823bca1e1567cf0a
Jesse <jesse.lare@gmail.com>
2023-11-03 22:46:23 -0400

    updating init.sql, adjusting varchar limit, and drop database line

--- db/init.sql
+++ db/init.sql
@@ -1,28 +1,32 @@
 
+DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 -- Create department table
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 -- Create role table
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
 );
 
 -- Create employee table
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
