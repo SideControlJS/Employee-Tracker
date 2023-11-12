b939fcf118370e210ca36c884446f9f0743c15ae
Jesse <jesse.lare@gmail.com>
2023-11-12 18:31:30 -0500

    fixing table creation

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,27 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 -- Create department table
-CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
 -- Create role table
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 -- Create employee table
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role_id INT,
-    manager_id INT NULL, -- Allows NULL values for cases where there is no manager
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
